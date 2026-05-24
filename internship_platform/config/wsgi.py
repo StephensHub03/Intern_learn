6321650ac8bfa87bb91c707854d1499e8da7f2fd
StephensHub03 <jeyastephen025@gmail.com>
2026-05-24 23:09:56 +0530

    Create production admin

--- internship_platform/config/wsgi.py
+++ internship_platform/config/wsgi.py
@@ -7,3 +7,13 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 
 application = get_wsgi_application()
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username="stephen").exists():
+    User.objects.create_superuser(
+        username="admin",
+        email="admin@gmail.com",
+        password="admin123"
+    )
